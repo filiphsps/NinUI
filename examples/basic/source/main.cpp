c2b3ce6a51e07311aa367fcebf08ea8a8d58b5a6
Filiph Sandström <filfat@hotmail.se>
2015-03-12 02:11:28 +0100

    Added an example

---
+++ examples/basic/source/main.cpp
@@ -0,0 +1,31 @@
+#include <string.h>
+
+#include <3ds.h>
+#include <3DS_UI.h>
+
+int main()
+{
+	gfxInitDefault();
+	gfxSet3D(true);
+	uiInit();
+	
+	uiWindow window1 =  createWindow();
+	
+	while (aptMainLoop())
+	{
+		gspWaitForVBlank();
+		hidScanInput();
+		
+		
+
+		u32 kDown = hidKeysDown();
+		if (kDown & KEY_START)
+			break; // break in order to return to hbmenu
+		
+		gfxFlushBuffers();
+		gfxSwapBuffers();
+	}
+
+	gfxExit();
+	return 0;
+}
